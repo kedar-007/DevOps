4afb713011bc5793a57055ecd464c80652a6afe4
kedar-007 <cakedar7390@gmail.com>
2025-09-29 18:04:53 +0530

    added the health check route

--- docker-1/index.js
+++ docker-1/index.js
@@ -6,5 +6,7 @@ app.get("/",(req,res) =>{
     console.log("hello docker")
     res.send("Hello kedar with the docker")
 });
-
+app.get("health-chcek", (req, res) => {
+    res.send("Running")
+  })
 app.listen(3000);
