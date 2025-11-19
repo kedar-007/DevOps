ac485c08e63fed2f50c1fefd759d9a2ba7a16575
kedar-007 <cakedar7390@gmail.com>
2025-11-19 16:38:02 +0530

    ADDED THE PROMETHEUS AND GRAFANA MONITORING

---
+++ promethous-2/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { middleware } from "./middleware";
+import client from "prom-client";
+import { metricsMiddleware } from "./metrics";
+
+const app = express();
+
+app.use(express.json());
+app.use(middleware);
+app.use(metricsMiddleware);
+
+app.get("/user", async (req, res) => {
+
+    await new Promise((resolve) => setTimeout(resolve, Math.random()*2500));
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
+});
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
+
+app.listen(3000,() =>{
+    console.log("Server running on the port 3000")
+});
